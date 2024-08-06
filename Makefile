6ad48855a81e2d3f7d5ca2fec126c55fde745959
Tommy Jeff <threcius@yahoo.com>
2024-08-06 12:39:58 +0800

    Simple route handler works

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 PROJECT = simple_web
 PROJECT_DESCRIPTION = New project
 PROJECT_VERSION = 0.1.0
-
 REL_DEPS += relx
+DEPS = cowboy
+dep_cowboy_commit = master
+DEP_PLUGINS = cowboy
+
 include erlang.mk
--- src/simple_web_app.erl
+++ src/simple_web_app.erl
@@ -5,7 +5,14 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-	simple_web_sup:start_link().
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", hello_handler, []}]}
+    ]),
+    {ok, _} = cowboy:start_clear(my_http_listener,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    simple_web_sup:start_link().
 
 stop(_State) ->
 	ok.
